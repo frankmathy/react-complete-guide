c72d764abb0ef6244ac72d4316e2aae83635ef85
Frank Mathy <frankmathy@gmail.com>
2019-09-24 22:40:05 +0200

    Removed Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -43,11 +42,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -66,10 +61,6 @@ class App extends Component {
       </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     const classes = [];
@@ -80,18 +71,16 @@ class App extends Component {
     }
 
     return  (
-        <StyleRoot>
-          <div className="App">
-            <h1>Hi, I'm a React app</h1>
-            <p className={classes.join(' ')}>This is really working!</p>
-            <button 
-              style={style}
-              onClick={this.togglePersonsHandler}>Toggle Persons</button>
-              {persons}
-          </div>
-        </StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React app</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+            {persons}
+        </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
