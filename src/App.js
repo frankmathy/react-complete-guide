2b54a6863853a7158742f97a7f3b51381d425a43
Frank Mathy <frankmathy@gmail.com>
2019-09-24 16:59:52 +0200

    Implemented conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 },
     ],
-    otherState: 'some other value'
-  });
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  const switchNameHandler = (newName) => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
-    setPersonsState({
+    this.setState({
       persons: [
         { name: 'Max', age: 28 },
         { name: 'Manu', age: 29 },
@@ -23,8 +24,8 @@ const App = props => {
     })
   }
 
-  const nameChangedHandler = (event) => {
-    setPersonsState({
+  nameChangedHandler = (event) => {
+    this.setState({
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
@@ -33,36 +34,45 @@ const App = props => {
     })
   }
 
-  console.log(personsState);
+  togglePersonsHandler = () => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons: !doesShow});
+  }
 
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer'
-  };
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
 
-  return  (
-      <div className="App">
-        <h1>Hi, I'm a React app</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={switchNameHandler.bind(this, "Maximilian")}>Switch Name</button>
-        <Person 
-          name={personsState.persons[0].name} 
-          age={personsState.persons[0].age}/>
-        <Person 
-          name={personsState.persons[1].name} 
-          age={personsState.persons[1].age}
-          click={switchNameHandler.bind(this, "Max!")}
-          changed={nameChangedHandler}>My Hobbies: Racing</Person>
-        <Person 
-          name={personsState.persons[2].name} 
-          age={personsState.persons[2].age}/>
-      </div>
-  );
+    return  (
+        <div className="App">
+          <h1>Hi, I'm a React app</h1>
+          <p>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}>Switch Name</button>
+          { this.state.showPersons === true ? 
+            <div >
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}/>
+              <Person 
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, "Max!")}
+                changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+              <Person 
+                name={this.state.persons[2].name} 
+                age={this.state.persons[2].age}/>
+            </div> : null
+          }
+        </div>
+    );
+  }
 }
 
 export default App;
